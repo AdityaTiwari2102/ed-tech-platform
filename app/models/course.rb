632416e21d2c5c20057b76d4ad46a9dcd6eecd3f
Aditya Tiwari <taditya.tiwari007@gmail.com>
2024-05-05 16:42:12 +0530

    Feat: Course enum and validations

---
+++ app/models/course.rb
@@ -0,0 +1,12 @@
+class Course < ApplicationRecord
+  enum status: { live: 'live', inactive: 'inactive' }
+  enum level: { beginner: 'beginner', intermediate: 'intermediate', advanced: 'advanced' }
+  enum category: { web_development: 'web_development', mobile_development: 'mobile_development', data_science: 'data_science', design: 'design' }
+
+  validates :title, presence: true, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 1000 }
+  validates :start_date, comparison: { greater_than_or_equal_to: Date.current }
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :description, length: { maximum: 100 }, allow_blank: true
+  validates :category, :level, :status, presence: true
+end
