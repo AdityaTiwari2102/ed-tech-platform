6fc6e49d52489aab93ca734aa4237a9b4eea8ae8
Aditya Tiwari <taditya.tiwari007@gmail.com>
2024-05-05 19:23:08 +0530

    Course CRUD (#1)
    * Feat: Course table migration
    
    * Feat: Course enum and validations
    
    * Feat: Course factory and seed
    
    * Feat: Course controller for CRUD operations
    
    * Feat: Course views for CRUD operations
    
    * Feat: Rspec tests for courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_104044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title", null: false, comment: "Course title"
+    t.string "description", comment: "Course description"
+    t.integer "price", null: false, comment: "Price of the course in INR. Must be greater than or equal to 1000."
+    t.date "start_date", null: false, comment: "Course commencement date"
+    t.date "end_date", null: false, comment: "Course completion date"
+    t.string "level", default: "beginner", null: false, comment: "Course level: beginner, intermediate, advanced"
+    t.string "category", default: "web_development", null: false, comment: "Course category: web_development, mobile_development, data_science, design"
+    t.string "status", default: "live", null: false, comment: "Course status: live, inactive"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_courses_on_category"
+    t.index ["level"], name: "index_courses_on_level"
+    t.index ["status"], name: "index_courses_on_status"
+    t.index ["title"], name: "index_courses_on_title", unique: true
+  end
 
 end
---
+++ spec/views/courses/show.html.tailwindcss_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "courses/show", type: :view do
+  before(:each) do
+    assign(:course, FactoryBot.create(:course))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/Description/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/Level/)
+    expect(rendered).to match(/Category/)
+    expect(rendered).to match(/Status/)
+  end
+end
